6a006dea32a32a42530c80b3a029b7c6a2b341ef
Olia Pankiv <o.pankiv@tenantcloud.com>
2022-02-06 17:06:14 +0200

    Added lifecycle hooks & ability to remove posts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,15 +15,21 @@ export class AppComponent {
 	posts: Post[] = [
 		{
 			title: 'What to learn Angular components',
-			text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, reprehenderit.'
+			text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, reprehenderit.',
+			id: 1
 		},
 		{
 			title: 'Angular is awesome',
-			text: 'Lorem ipsum dolor sit amet.'
+			text: 'Lorem ipsum dolor sit amet.',
+			id: 2
 		}
 	]
 
 	public onUpdatePosts(post: Post) {
 		this.posts.unshift(post);
+	}
+
+	public onRemovePost(id: number) {
+		this.posts = this.posts.filter(post => post.id !== id)
 	}
 }
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,4 +1,15 @@
-import { Component, ContentChild, ElementRef, Input, OnInit } from '@angular/core';
+import {
+	AfterContentChecked,
+	AfterContentInit, AfterViewChecked, AfterViewInit,
+	Component,
+	ContentChild,
+	DoCheck,
+	ElementRef, EventEmitter,
+	Input,
+	OnChanges, OnDestroy,
+	OnInit, Output,
+	SimpleChanges
+} from '@angular/core';
 import { Post } from '../app.component';
 
 @Component({
@@ -6,14 +17,48 @@ import { Post } from '../app.component';
 	templateUrl: './post.component.html',
 	styleUrls: ['./post.component.scss']
 })
-export class PostComponent implements OnInit {
+export class PostComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy{
 	@Input() public post!: Post;
+	@Output() onRemove: EventEmitter<number> = new EventEmitter<number>();
 	@ContentChild('info', {static: true}) infoRef!: ElementRef;
 
-	constructor() { }
-
-	ngOnInit() {
-		console.log(this.infoRef)
+	constructor() {
+		// console.log('constructor')
 	}
 
+	public ngOnChanges(changes: SimpleChanges) {
+		// console.log('ngOnChanges', changes)
+	}
+
+	public ngOnInit() {
+		// console.log('ngOnInit')
+	}
+
+	public ngDoCheck() {
+		// console.log('ngDoCheck')
+	}
+
+	public ngAfterContentInit() {
+		// console.log('ngAfterContentInit')
+	}
+
+	public ngAfterContentChecked() {
+		// console.log('ngAfterContentChecked')
+	}
+
+	public ngAfterViewInit() {
+		// console.log('ngAfterViewInit')
+	}
+
+	public ngAfterViewChecked() {
+		// console.log('ngAfterViewChecked')
+	}
+
+	public ngOnDestroy() {
+		// console.log('ngOnDestroy')
+	}
+
+	public onRemovePost() {
+		this.onRemove.emit(this.post.id)
+	}
 }
