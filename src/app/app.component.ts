63301b46135f33cc5e1ffb5610e5dcda899f0757
Olia Pankiv <o.pankiv@tenantcloud.com>
2022-02-06 16:31:06 +0200

    Added ability to add posts dynamically

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,29 @@
 import { Component } from '@angular/core';
 
 export interface Post {
-  title: string;
-  text: string;
-  id?: number;
+	title: string;
+	text: string;
+	id?: number;
 }
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  posts: Post[] = [
-    {
-      title: 'What to learn Angular components',
-      text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, reprehenderit.'
-    },
-    {
-      title: 'Angular is awesome',
-      text: 'Lorem ipsum dolor sit amet.'
-    }
-  ]
+	posts: Post[] = [
+		{
+			title: 'What to learn Angular components',
+			text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, reprehenderit.'
+		},
+		{
+			title: 'Angular is awesome',
+			text: 'Lorem ipsum dolor sit amet.'
+		}
+	]
+
+	public onUpdatePosts(post: Post) {
+		this.posts.unshift(post);
+	}
 }
--- src/app/post-form/post-form.component.ts
+++ src/app/post-form/post-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Post } from '../app.component';
 
 @Component({
 	selector: 'app-post-form',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./post-form.component.scss']
 })
 export class PostFormComponent implements OnInit {
+  @Output() public onAdd: EventEmitter<Post> = new EventEmitter<Post>();
+
+	public title = '';
+	public text = '';
 
 	constructor() { }
 
-	ngOnInit() {
+	ngOnInit() {}
+
+	public onAddPost() {
+		if (this.title.trim() && this.title.trim()) {
+			const post: Post = {
+				title: this.title,
+				text: this.text
+			}
+
+			this.onAdd.emit(post);
+
+			this.title = this.text = '';
+		}
 	}
 
 }
