0af81cdf7abe3b0fc85a65f2f775dfc13f2d5aa1
Olia Pankiv <o.pankiv@tenantcloud.com>
2022-02-06 16:01:08 +0200

    Added post interface and some posts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+  title: string;
+  text: string;
+  id?: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-basics';
+  posts: Post[] = [
+    {
+      title: 'What to learn Angular components',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, reprehenderit.'
+    },
+    {
+      title: 'Angular is awesome',
+      text: 'Lorem ipsum dolor sit amet.'
+    }
+  ]
 }
