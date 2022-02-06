13ac895cabae7deaaa25ef121aaff3e03555d63b
Olia Pankiv <o.pankiv@tenantcloud.com>
2022-02-06 18:07:43 +0200

    Added HostBinding

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, ElementRef, HostListener, Input, Renderer2} from '@angular/core'
+import { Directive, ElementRef, HostBinding, HostListener, Input, Renderer2 } from '@angular/core'
 
 @Directive({
 	selector: '[appStyle]'
@@ -7,20 +7,24 @@ export class StyleDirective {
 	@Input('appStyle') color: string = 'blue';
 	@Input() styles!: { border?: string, fontWeight?: string, borderRadius?: string };
 
+	@HostBinding('style.color') elColor!: string | null;
+
 	@HostListener('mouseenter')
 	onEnter() {
-		this.r.setStyle(this.el.nativeElement, 'color', this.color)
-		this.r.setStyle(this.el.nativeElement, 'font-weight', this.styles.fontWeight)
-		this.r.setStyle(this.el.nativeElement, 'border', this.styles.border)
-		this.r.setStyle(this.el.nativeElement, 'border-radius', this.styles.borderRadius)
+	  this.elColor = this.color;
+		// this.r.setStyle(this.el.nativeElement, 'color', this.color)
+		// this.r.setStyle(this.el.nativeElement, 'font-weight', this.styles.fontWeight)
+		// this.r.setStyle(this.el.nativeElement, 'border', this.styles.border)
+		// this.r.setStyle(this.el.nativeElement, 'border-radius', this.styles.borderRadius)
 	}
 
 	@HostListener('mouseleave')
 	onLeave() {
-		this.r.setStyle(this.el.nativeElement, 'color', null)
-		this.r.setStyle(this.el.nativeElement, 'font-weight', null)
-		this.r.setStyle(this.el.nativeElement, 'border', null)
-		this.r.setStyle(this.el.nativeElement, 'border-radius', null)
+    this.elColor = null;
+    // this.r.setStyle(this.el.nativeElement, 'color', null)
+		// this.r.setStyle(this.el.nativeElement, 'font-weight', null)
+		// this.r.setStyle(this.el.nativeElement, 'border', null)
+		// this.r.setStyle(this.el.nativeElement, 'border-radius', null)
 	}
 
 	constructor(private el: ElementRef, private r: Renderer2) {}
