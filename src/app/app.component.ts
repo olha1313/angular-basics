703426d893a8de85c73fe34077390dcaaff735b2
Olia Pankiv <o.pankiv@tenantcloud.com>
2022-03-05 10:26:12 +0200

    Added pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,37 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+	title: string,
+	text: string,
+}
+
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+	public e: number = Math.E;
+	public str = 'hello world'
+	public date: Date = new Date();
+	public float = 0.42;
+	public obj = {
+		a: 1,
+		b: {
+			c:2,
+			b: {
+				e: 3,
+				f: 4,
+			}
+		}
+	};
+
+	search = '';
+	searchField: string = 'title';
+
+	posts: Post[] = [
+		{title: 'Javascript', text: 'The best language'},
+		{title: 'Typescript', text: 'Is awesome'},
+		{title: 'HTML', text: 'Cool!'}
+	]
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MultByPipe } from './pipes/mult-by.pipe';
+import { FormsModule } from '@angular/forms';
+import { FilterPipe } from './pipes/filter.pipe';
 
 @NgModule({
 	declarations: [
-		AppComponent
+		AppComponent,
+		MultByPipe,
+  FilterPipe
 	],
 	imports: [
-		BrowserModule
+		BrowserModule,
+		FormsModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
