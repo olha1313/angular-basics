217d6f5d05fa9eb1b1ef82ff6048d4629ce1f4d0
Olia Pankiv <o.pankiv@tenantcloud.com>
2022-03-05 12:34:44 +0200

    Added services

---
+++ src/app/services/app-counter.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {LogService} from './log.service';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class AppCounterService {
+	public counter = 0;
+
+	constructor(private logService: LogService) {}
+
+	public increase() {
+		this.logService.log('increase counter....');
+		this.counter++
+	}
+
+	public decrease() {
+		this.logService.log('decrease counter....');
+		this.counter--
+	}
+}
