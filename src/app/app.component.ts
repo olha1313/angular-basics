026e6d35baaf3bd49a8ba8dbc7d0b1fd479a0105
Olia Pankiv <o.pankiv@tenantcloud.com>
2022-03-05 14:09:38 +0200

    Added form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+	public form!: FormGroup;
+
+	public ngOnInit(): void {
+			this.form = new FormGroup({
+				email: new FormControl('', [Validators.email, Validators.required]),
+				password: new FormControl('', [Validators.required, Validators.minLength(4)])
+			})
+	}
+
+	public submit() {
+		if(this.form.valid) {
+			console.log('Form', this.form);
+			const formData = { ...this.form.value };
+
+			console.log(formData)
+		}
+	}
+}
