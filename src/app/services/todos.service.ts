407d47d55f25a0e70653da8438f8d4a338e429f4
Olia Pankiv <o.pankiv@tenantcloud.com>
2022-03-08 19:44:51 +0200

    Added query parameters

--- src/app/services/todos.service.ts
+++ src/app/services/todos.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { catchError, delay, Observable, throwError } from 'rxjs';
 
 export interface Todo {
@@ -13,18 +13,25 @@ export interface Todo {
 })
 export class TodosService {
 	public url = 'https://jsonplaceholder.typicode.com/todos';
-	public getUrl = 'https://jsonplaceholder.typicode.com/todos?_limit=2';
+	public getUrl = 'https://jsonplaceholder.typicode.com/todos';
 
 	constructor(private http: HttpClient) {}
 
 	public addTodo(todo: Todo): Observable<Todo> {
 		return this.http.post<Todo>(this.url, todo, {
-		  headers: new HttpHeaders({ 'myHeader': Math.random().toString() })
+			headers: new HttpHeaders({ 'myHeader': Math.random().toString() })
 		})
 	}
 
 	public fetchTodos(): Observable<Todo[]> {
-		return this.http.get<Todo[]>(this.getUrl)
+		let params = new HttpParams();
+		params = params.append('_limit', '4');
+		params = params.append('custom', 'anything');
+
+		return this.http.get<Todo[]>(this.getUrl, {
+			// params: new HttpParams().set('_limit', '4')
+			params
+		})
 			.pipe(delay(500))
 	}
 
