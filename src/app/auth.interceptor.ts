510d356374b08ab5d5806113150dd85536d9d501
Olia Pankiv <o.pankiv@tenantcloud.com>
2022-03-08 19:46:08 +0200

    Added clone request to interceptor

--- src/app/auth.interceptor.ts
+++ src/app/auth.interceptor.ts
@@ -1,9 +1,21 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpEvent, HttpEventType, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
 
 export class AuthInterceptor implements HttpInterceptor {
 	intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 		console.log('intercept', req);
-		return next.handle(req);
+
+		const clone = req.clone({
+			headers: req.headers.append('Auth', 'Some random token')
+		});
+
+		return next.handle(clone)
+			.pipe(
+				tap(event => {
+					if (event.type === HttpEventType.Response) {
+						console.log('Interceptor response event', event)
+					}
+				})
+			);
 	}
 }
