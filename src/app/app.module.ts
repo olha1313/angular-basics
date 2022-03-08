f8e77253552afee32ac21ce530bab2ccb81bd030
Olia Pankiv <o.pankiv@tenantcloud.com>
2022-03-08 20:15:08 +0200

    Added structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { FormsModule } from '@angular/forms'
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
+import { HomePageComponent } from './home-page/home-page.component'
+import { AppRoutingModule } from './app-routing.module'
+import { AboutPageComponent } from './about-page/about-page.component'
+import { AboutExtraPageComponent } from './about-page/about-extra-page/about-extra-page.component'
+import { ColorDirective } from './shared/color.directive'
+import { PageNamePipe } from './shared/page-name.pipe'
 
 @NgModule({
 	declarations: [
-		AppComponent
+		AppComponent,
+		HomePageComponent,
+		AboutPageComponent,
+		AboutExtraPageComponent,
+		ColorDirective,
+		PageNamePipe
 	],
 	imports: [
-		BrowserModule
+		BrowserModule,
+		FormsModule,
+		AppRoutingModule,
 	],
 	providers: [],
 	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
