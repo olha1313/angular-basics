ed1777e095a8304ec224e36bd417f9ec1a7423b1
Olia Pankiv <o.pankiv@tenantcloud.com>
2022-03-08 20:15:44 +0200

    Added about page module

---
+++ src/app/about-page/about-page.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
+import { AboutPageComponent } from './about-page.component';
+import { AboutExtraPageComponent } from './about-extra-page/about-extra-page.component';
+
+@NgModule({
+	declarations: [
+		AboutPageComponent,
+		AboutExtraPageComponent,
+	],
+	imports: [
+		SharedModule,
+		CommonModule,
+		RouterModule.forChild([
+			{ path: 'about', component: AboutPageComponent, children: [
+					{ path: 'extra', component: AboutExtraPageComponent }
+				]
+			},
+		])
+	],
+	exports: [
+		RouterModule
+	]
+})
+export class AboutPageModule {
+
+}
