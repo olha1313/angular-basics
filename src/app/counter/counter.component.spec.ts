b4cbc4a57ecfd1decf5f0a762f0b223c529bafc4
Olia Pankiv <o.pankiv@tenantcloud.com>
2022-03-09 21:25:21 +0200

    Added tests for eventEmitter & form

--- src/app/counter/counter.component.spec.ts
+++ src/app/counter/counter.component.spec.ts
@@ -1,10 +1,11 @@
+import { FormBuilder } from '@angular/forms';
 import { CounterComponent } from './counter.component';
 
 describe('CounterComponent', () => {
 	let component: CounterComponent;
 
 	beforeEach(() => {
-		component = new CounterComponent()
+		component = new CounterComponent(new FormBuilder())
 	})
 
 	it('should increment counter by 1', () => {
@@ -16,4 +17,23 @@ describe('CounterComponent', () => {
 		component.decrement();
 		expect(component.counter).toBe(-1);
 	})
+
+	it('should increment value by event emitter', () => {
+		let result = 0;
+		component.counterEmitter.subscribe(v => result = v)
+		component.increment();
+		expect(result).toBe(1);
+	})
+
+	it('should create form with two controls', () => {
+		expect(component.form.contains('email')).toBeTruthy();
+		expect(component.form.contains('login')).toBeTruthy();
+	});
+
+	it('should mark login as invalid if empty value', () => {
+		const control = component.form.get('login');
+		control?.setValue('');
+
+		expect(control?.valid).toBeFalsy();
+	});
 })
--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,14 +1,26 @@
-import {Component} from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
 	selector: 'app-counter',
 	template: `Counter: {{ counter}}`
 })
 export class CounterComponent {
+	@Output() counterEmitter = new EventEmitter<number>();
+
 	public counter = 0;
+	public form!: FormGroup;
+
+	constructor(public formBuilder: FormBuilder) {
+		this.form = formBuilder.group({
+			login: ['', Validators.required],
+			email: ['']
+		})
+	}
 
 	public increment(): void {
 		this.counter++;
+		this.counterEmitter.emit(this.counter);
 	}
 
 	public decrement(): void {
