8236bef9a73d340dc0849894f53702d001318622
Olia Pankiv <o.pankiv@tenantcloud.com>
2022-03-13 21:22:01 +0200

    Added automated generated component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, ComponentFactoryResolver, ViewChild} from '@angular/core';
+import { ModalComponent } from './modal/modal.component';
+import { RefDirective } from './ref.directive';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+	@ViewChild(RefDirective) refDir!: RefDirective;
+
+	constructor(
+		private resolver: ComponentFactoryResolver
+	) {}
+
+	public openModal(): void {
+		const modalFactory = this.resolver.resolveComponentFactory(ModalComponent);
+		this.refDir.containerRef.clear();
+
+		const component = this.refDir.containerRef.createComponent(modalFactory);
+
+		component.instance.title = 'Sraka';
+		component.instance.close.subscribe(() => {
+			this.refDir.containerRef.clear();
+		});
+	}
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ModalComponent } from './modal/modal.component';
+import { RefDirective } from './ref.directive';
 
 @NgModule({
 	declarations: [
-		AppComponent
+		AppComponent,
+		ModalComponent,
+		RefDirective
 	],
 	imports: [
 		BrowserModule
 	],
 	providers: [],
+	entryComponents: [ModalComponent],
 	bootstrap: [AppComponent]
 })
 export class AppModule { }
